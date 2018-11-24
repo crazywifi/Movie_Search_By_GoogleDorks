d4025eb31c048d53bfcf110de697bde1f3a921f3
unknown <rishabh2241991@gmail.com>
2018-11-24 16:43:20 +0530

    first commit

---
+++ Movie_Search_By_Name_Google.py
@@ -0,0 +1,29 @@
+
+try:
+    from googlesearch import search
+except ImportError:
+    print("No module named 'google' found")
+
+# to search
+movie = raw_input("Enter Movie Name: ")
+
+query = "site:vidoza.net "+movie
+print query
+for j in search(query, tld="com", num=10, stop=1, pause=2):
+    print(j)
+
+query = "site:filerio.in "+movie
+print query
+for j in search(query, tld="com", num=10, stop=1, pause=2):
+    print(j)
+
+query = "site:uptobox.com "+movie
+print query
+for j in search(query, tld="com", num=10, stop=1, pause=2):
+    print(j)
+
+query = "site:userscloud.com "+movie
+print query
+for j in search(query, tld="com", num=10, stop=1, pause=2):
+    print(j)
+
